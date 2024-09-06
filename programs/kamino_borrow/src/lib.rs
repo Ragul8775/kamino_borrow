da77964e0b0d53a2c46c203883d2d389cb352b5a
Ragul8775 <rb7439@srmist.edu.in>
2024-09-06 22:50:43 +0530

    Check

--- programs/kamino_borrow/src/lib.rs
+++ programs/kamino_borrow/src/lib.rs
@@ -1,6 +1,5 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::instruction::Instruction;
-use anchor_spl::token::{Token, TokenAccount};
 
 declare_id!("3DSox9PrsL3PTJDuNq5p4kCr9TkfVFN8dAhMffJ6DiBr");
 
@@ -10,14 +9,14 @@ pub mod kamino_borrow {
     use anchor_lang::solana_program::program::invoke;
 
     pub fn execute_kamino_borrow(
-        ctx: Context<ExecuteKaminoBorrow>,
-        liquidity_amount: u64,
+        ctx: Context<ExecuteKaminoOperations>,
+        data: Vec<Vec<u8>>,
     ) -> Result<()> {
-        // Refresh the reserve for the collateral
         invoke(
-            &refresh_reserve_instruction(
+            &initiate_ixns(
                 &ctx.accounts.kamino_program.key(),
                 &ctx.remaining_accounts,
+                data[0].clone(),
             ),
             &[
                 ctx.remaining_accounts[0].clone(),
@@ -29,19 +28,37 @@ pub mod kamino_borrow {
             ],
         )?;
 
+        // Refresh the reserve for the collateral
+        invoke(
+            &&refresh_reserve_instruction_collateral(
+                &ctx.accounts.kamino_program.key(),
+                &ctx.remaining_accounts,
+                data[1].clone(),
+            ),
+            &[
+                ctx.remaining_accounts[6].clone(),
+                ctx.remaining_accounts[7].clone(),
+                ctx.remaining_accounts[8].clone(),
+                ctx.remaining_accounts[9].clone(),
+                ctx.remaining_accounts[10].clone(),
+                ctx.remaining_accounts[11].clone(),
+            ],
+        )?;
+
         //refresh reserve for the borrow
         invoke(
             &refresh_reserve_instruction(
                 &ctx.accounts.kamino_program.key(),
                 &ctx.remaining_accounts,
+                data[2].clone(),
             ),
             &[
-                ctx.remaining_accounts[6].clone(),
-                ctx.remaining_accounts[1].clone(),
-                ctx.remaining_accounts[2].clone(),
-                ctx.remaining_accounts[3].clone(),
-                ctx.remaining_accounts[4].clone(),
-                ctx.remaining_accounts[5].clone(),
+                ctx.remaining_accounts[12].clone(),
+                ctx.remaining_accounts[13].clone(),
+                ctx.remaining_accounts[14].clone(),
+                ctx.remaining_accounts[15].clone(),
+                ctx.remaining_accounts[16].clone(),
+                ctx.remaining_accounts[17].clone(),
             ],
         )?;
 
@@ -50,10 +67,11 @@ pub mod kamino_borrow {
             &refresh_obligation_instruction(
                 &ctx.accounts.kamino_program.key(),
                 &ctx.remaining_accounts,
+                data[3].clone(),
             ),
             &[
-                ctx.remaining_accounts[1].clone(),
-                ctx.remaining_accounts[7].clone(),
+                ctx.remaining_accounts[18].clone(),
+                ctx.remaining_accounts[19].clone(),
             ],
         )?;
 
@@ -62,20 +80,20 @@ pub mod kamino_borrow {
             &borrow_liquidity_instruction(
                 &ctx.accounts.kamino_program.key(),
                 ctx.remaining_accounts,
-                liquidity_amount,
+                data[4].clone(),
             ),
             &[
-                ctx.accounts.owner.to_account_info(),
-                ctx.remaining_accounts[7].clone(),
-                ctx.remaining_accounts[1].clone(),
-                ctx.remaining_accounts[8].clone(),
-                ctx.remaining_accounts[9].clone(),
-                ctx.remaining_accounts[10].clone(),
-                ctx.remaining_accounts[11].clone(),
-                ctx.remaining_accounts[12].clone(),
-                ctx.accounts.user_destination_liquidity.to_account_info(),
-                ctx.accounts.token_program.to_account_info(),
-                ctx.accounts.instruction_sysvar.to_account_info(),
+                ctx.remaining_accounts[20].clone(),
+                ctx.remaining_accounts[21].clone(),
+                ctx.remaining_accounts[22].clone(),
+                ctx.remaining_accounts[23].clone(),
+                ctx.remaining_accounts[24].clone(),
+                ctx.remaining_accounts[25].clone(),
+                ctx.remaining_accounts[26].clone(),
+                ctx.remaining_accounts[27].clone(),
+                ctx.remaining_accounts[28].clone(),
+                ctx.remaining_accounts[29].clone(),
+                ctx.remaining_accounts[30].clone(),
             ],
         )?;
         Ok(())
@@ -83,77 +101,103 @@ pub mod kamino_borrow {
 }
 
 #[derive(Accounts)]
-pub struct ExecuteKaminoBorrow<'info> {
-    #[account(mut)]
-    pub owner: Signer<'info>,
-    #[account(mut)]
-    pub fee_payer: Signer<'info>,
-    #[account(mut)]
-    pub user_destination_liquidity: Account<'info, TokenAccount>,
-    /// CHECK: This is safe because it is the Token Program required for SPL tokens
-    #[account(mut)]
-    pub token_program: AccountInfo<'info>,
-    /// CHECK: This is safe because it's the instruction sysvar for the program
-    #[account(mut)]
-    pub instruction_sysvar: AccountInfo<'info>,
-    /// CHECK: This is safe because we are interacting with the Kamino program
-    #[account(mut)]
+pub struct ExecuteKaminoOperations<'info> {
+    /// CHECK: This account is not directly used by the program and is passed to CPI. Safe because we trust the external program's account validation.
     pub kamino_program: AccountInfo<'info>,
+}
+
+fn initiate_ixns(
+    kamino_program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    data: Vec<u8>,
+) -> Instruction {
+    Instruction {
+        program_id: *kamino_program_id,
+        accounts: vec![
+            AccountMeta::new_readonly(accounts[0].key(), true),
+            AccountMeta::new(accounts[1].key(), true),
+            AccountMeta::new(accounts[2].key(), false),
+            AccountMeta::new_readonly(accounts[3].key(), false),
+            AccountMeta::new_readonly(accounts[4].key(), false),
+            AccountMeta::new_readonly(accounts[5].key(), false),
+        ],
+        data,
+    }
 }
 
 fn refresh_reserve_instruction(
     kamino_program_id: &Pubkey,
     remaining_accounts: &[AccountInfo],
+    data: Vec<u8>,
 ) -> Instruction {
     Instruction {
         program_id: *kamino_program_id,
         accounts: vec![
-            AccountMeta::new(*remaining_accounts[0].key, false),
-            AccountMeta::new(*remaining_accounts[1].key, false),
-            AccountMeta::new(*remaining_accounts[2].key, false),
-            AccountMeta::new(*remaining_accounts[3].key, false),
-            AccountMeta::new(*remaining_accounts[4].key, false),
-            AccountMeta::new(*remaining_accounts[5].key, false),
+            AccountMeta::new_readonly(remaining_accounts[6].key(), true),
+            AccountMeta::new(*remaining_accounts[7].key, false),
+            AccountMeta::new(*remaining_accounts[8].key, false),
+            AccountMeta::new(*remaining_accounts[9].key, false),
+            AccountMeta::new(*remaining_accounts[10].key, false),
+            AccountMeta::new(*remaining_accounts[11].key, false),
+            AccountMeta::new(*remaining_accounts[12].key, false),
         ],
-        data: vec![0],
+        data,
+    }
+}
+fn refresh_reserve_instruction_collateral(
+    kamino_program_id: &Pubkey,
+    remaining_accounts: &[AccountInfo],
+    data: Vec<u8>,
+) -> Instruction {
+    Instruction {
+        program_id: *kamino_program_id,
+        accounts: vec![
+            AccountMeta::new_readonly(remaining_accounts[13].key(), true),
+            AccountMeta::new(*remaining_accounts[14].key, false),
+            AccountMeta::new(*remaining_accounts[15].key, false),
+            AccountMeta::new(*remaining_accounts[16].key, false),
+            AccountMeta::new(*remaining_accounts[17].key, false),
+            AccountMeta::new(*remaining_accounts[18].key, false),
+            AccountMeta::new(*remaining_accounts[19].key, false),
+        ],
+        data,
     }
 }
 
 fn refresh_obligation_instruction(
     kamino_program_id: &Pubkey,
     remaining_accounts: &[AccountInfo],
+    data: Vec<u8>,
 ) -> Instruction {
     Instruction {
         program_id: *kamino_program_id,
         accounts: vec![
-            AccountMeta::new(*remaining_accounts[1].key, false),
-            AccountMeta::new(*remaining_accounts[7].key, false),
+            AccountMeta::new(*remaining_accounts[20].key, false),
+            AccountMeta::new(*remaining_accounts[21].key, false),
         ],
-        data: vec![1],
+        data,
     }
 }
 
 fn borrow_liquidity_instruction(
     kamino_program_id: &Pubkey,
     remaining_accounts: &[AccountInfo],
-    liquidity_amount: u64,
+    data: Vec<u8>,
 ) -> Instruction {
-    let mut data = vec![2];
-    data.extend_from_slice(&liquidity_amount.to_le_bytes());
     Instruction {
         program_id: *kamino_program_id,
         accounts: vec![
-            AccountMeta::new(*remaining_accounts[0].key, true),
-            AccountMeta::new(*remaining_accounts[7].key, false),
-            AccountMeta::new(*remaining_accounts[1].key, false),
-            AccountMeta::new(*remaining_accounts[8].key, false),
-            AccountMeta::new(*remaining_accounts[9].key, false),
-            AccountMeta::new(*remaining_accounts[10].key, false),
-            AccountMeta::new(*remaining_accounts[11].key, false),
-            AccountMeta::new(*remaining_accounts[12].key, false),
-            AccountMeta::new(*remaining_accounts[13].key, false),
-            AccountMeta::new_readonly(*remaining_accounts[14].key, false),
-            AccountMeta::new_readonly(*remaining_accounts[15].key, false),
+            AccountMeta::new(*remaining_accounts[22].key, true),
+            AccountMeta::new(*remaining_accounts[23].key, false),
+            AccountMeta::new(*remaining_accounts[24].key, false),
+            AccountMeta::new(*remaining_accounts[25].key, false),
+            AccountMeta::new(*remaining_accounts[26].key, false),
+            AccountMeta::new(*remaining_accounts[27].key, false),
+            AccountMeta::new(*remaining_accounts[28].key, false),
+            AccountMeta::new(*remaining_accounts[29].key, false),
+            AccountMeta::new(*remaining_accounts[30].key, false),
+            AccountMeta::new_readonly(*remaining_accounts[31].key, false),
+            AccountMeta::new_readonly(*remaining_accounts[32].key, false),
         ],
         data,
     }
--- tests/kamino_borrow.ts
+++ tests/kamino_borrow.ts
@@ -1,16 +1,89 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+
+import { BN } from "@coral-xyz/anchor";
+import {
+  DEFAULT_RECENT_SLOT_DURATION_MS,
+  KaminoAction,
+  KaminoMarket,
+  PROGRAM_ID,
+  VanillaObligation,
+} from "@kamino-finance/klend-sdk";
+import { assert } from "chai";
 import { KaminoBorrow } from "../target/types/kamino_borrow";
 
+const provider = anchor.AnchorProvider.env();
+anchor.setProvider(provider);
+
+const connection = new Connection(
+  "https://mainnet.helius-rpc.com/?api-key=91acf6dc-f1f0-4db8-9763-aff8b775fa6a"
+);
+
+const MAINNET_LENDING_MARKET = new PublicKey(
+  "7u3HeHxYDLhnCoErrtycNokbQYbWGzLs6JSDqGAv5PfF"
+);
+const KAMINO_PROGRAM = new PublicKey(
+  "KLend2g3cP87fffoy8q1mQqGKjrxjC8boSyAYavgmjD"
+);
+
 describe("kamino_borrow", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  let payer = Keypair.generate();
+  const deposit_amount = new BN(1 * 10 ** 9);
+  const half_deposit_amount = deposit_amount.div(new BN(2));
 
-  const program = anchor.workspace.KaminoBorrow as Program<KaminoBorrow>;
+  const program = anchor.workspace.KaminoBorrow as anchor.Program<KaminoBorrow>;
+  it("deposit", async () => {
+    try {
+      const kaminoMarket = await KaminoMarket.load(
+        connection,
+        MAINNET_LENDING_MARKET,
+        DEFAULT_RECENT_SLOT_DURATION_MS,
+        PROGRAM_ID,
+        true
+      );
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+      const kaminoAction = await KaminoAction.buildDepositTxns(
+        kaminoMarket,
+        deposit_amount,
+        new PublicKey("So11111111111111111111111111111111111111112"),
+        payer.publicKey,
+        new VanillaObligation(PROGRAM_ID),
+        1_000_000,
+        true
+      );
+
+      const borrowAction = await KaminoAction.buildBorrowTxns(
+        kaminoMarket!,
+        half_deposit_amount,
+        new PublicKey("So11111111111111111111111111111111111111112"),
+        payer.publicKey,
+        new VanillaObligation(PROGRAM_ID),
+        1000000,
+        true,
+        undefined,
+        true,
+        PublicKey.default
+      );
+      if (!borrowAction) {
+        throw new Error("Borrow Action not generated properly");
+      }
+
+      console.log("Borrow Instructions:", borrowAction.lendingIxs);
+
+      // Combine all instructions
+      const allInstructions = [
+        ...borrowAction.setupIxs,
+        ...borrowAction.lendingIxs,
+        ...borrowAction.cleanupIxs,
+      ];
+
+      const allAccountMetas = allInstructions.flatMap((ix) => ix.keys);
+      const ixDatas: Buffer[] = allInstructions.map((ix) => ix.data);
+
+      console.log("All Account Metas:", allAccountMetas);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   });
 });
